2536d0a6f0575f32094c5d87dbf98bef71d01cc0
James C-D <51571627+jamescd18@users.noreply.github.com>
2021-03-28 12:55:12 -0700

    Added sudo code outlines

--- rc-transmitter/rc-transmitter.ino
+++ rc-transmitter/rc-transmitter.ino
@@ -1,9 +1,21 @@
 void setup() {
   // put your setup code here, to run once:
 
+  // setup joystick potentiometer for left/right
+  // setup joystick potentiometer for forward/back
+
+  // setup for radio transmitter
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+  // check left/right value
+  // check forward/back value
+
+  // broadcast left/right value
+  // broadcast forward/back value
+
+  // wait some interval
 }
