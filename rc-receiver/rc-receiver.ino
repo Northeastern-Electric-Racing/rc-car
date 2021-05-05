ae5feef7a92bcb76de726e556cee313a53c90493
Jasper Kimball <kimball.j@northeastern.edu>
2021-05-04 22:50:27 -0400

    Used Der Chi's code and made modifications to it.

--- rc-receiver/rc-receiver.ino
+++ rc-receiver/rc-receiver.ino
@@ -1,22 +1,38 @@
+#include "RF24.h"
+
+RF24 radio(9,10);
+
+const int SERVO_PIN = 10;
+const int ESC_PIN = 11;
+
+int ReceivedMessage[1] = {000}; // Used to store value received by the NRF24L01
+
+const uint64_t pipe = 0xE6E6E6E6E6E6; // Needs to be the same for communicating between 2 NRF24L01 
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+    
+  radio.begin(); // Start the NRF24L01
+  radio.openReadingPipe(1, pipe); // Get NRF24L01 ready to receive
+  radio.startListening(); // Listen to see if information received
 
-  // setup reciever input
-
-  // setup output for steering servo
-
-  // setup output for ESC motor control
+  // setup output pins
+  pinMode(SERVO_PIN, OUTPUT);
+  pinMode(ESC_PIN, OUTPUT);
 
   // ESC arming sequence
-
+  digitalWrite(ESC_PIN, 255);
+  delay(500);
+  digitalWrite(ESC_PIN, 0);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  while (radio.available()) {
+    radio.read(ReceivedMessage, 1); // Read information from the NRF24L01
 
-  // parse reciever input
-
-  // set steering angle
-  // set motor power
-
+    // set motor power
+    digitalWrite(SERVO_PIN, ReceivedMessage[0]);
+    // set steering angle
+    digitalWrite(ESC_PIN, ReceivedMessage[1]);
+  }
 }
--- rc-transmitter/rc-transmitter.ino
+++ rc-transmitter/rc-transmitter.ino
@@ -1,21 +1,33 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "RF24.h" // https://nrf24.github.io/RF24/
 
-  // setup joystick potentiometer for left/right
-  // setup joystick potentiometer for forward/back
+#define PIN_ANALOG_X 0
+#define PIN_ANALOG_Y 1
 
-  // setup for radio transmitter
+RF24 radio(9, 10); 
 
+int SentMessage[1] = {000}; // Used to store value before being sent through the NRF24L01
+
+const uint64_t pipe = 0xE6E6E6E6E6E6; // Needs to be the same for communicating between 2 NRF24L01 
+
+void setup() { 
+  Serial.begin(9600);
+
+  radio.begin(); // Start the NRF24L01
+  radio.openWritingPipe(pipe); // Get NRF24L01 ready to transmit
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Print x axis values
+  Serial.print("x: ");
+  Serial.println(analogRead(PIN_ANALOG_X));
+  SentMessage[0] = analogRead(PIN_ANALOG_X);
+  radio.write(SentMessage, 1); // Send value through NRF24L01
 
-  // check left/right value
-  // check forward/back value
-
-  // broadcast left/right value
-  // broadcast forward/back value
-
-  // wait some interval
+  // Print y axis values
+  Serial.print("y: ");
+  Serial.println(analogRead(PIN_ANALOG_Y));
+  SentMessage[1] = analogRead(PIN_ANALOG_Y);
+  radio.write(SentMessage, 1);
+ 
+  delay(500);
 }
